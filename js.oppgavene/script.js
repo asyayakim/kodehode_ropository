f918acce4dfc6a8e1b92cb094f069b196cb14ea7
Asia <asyayakimova@yahoo.com>
2025-02-24 10:58:03 +0100

    Basic intro to js

---
+++ js.oppgavene/script.js
@@ -0,0 +1,35 @@
+// console.log("Hello, World!");
+// cntr + * + / = comment out
+// var x = 5;
+// var y = 10;
+// var z = x + y;
+// console.log(z);
+let a = 5;
+let b = 10;
+let c = a + b;
+console.log(c);
+const name = "John";
+b = 15;
+console.log(name + " is " + b + " years old.");
+let d = 30.6;
+isLogged = true;
+let obj = {
+  name: "John",
+  age: 30,
+  isLogged: true,
+};
+
+let user = {
+  username: "John",
+  userAge: 30,
+  userCity: "Oslo",
+}
+
+if (user.userAge > 18) {
+  console.log(user.username + " is an adult.");
+} else if (user.userAge === 18) {
+  console.log(user.username + " is 18 years old.");
+}
+else {
+  console.log(user.username + " is not an adult.");
+}
